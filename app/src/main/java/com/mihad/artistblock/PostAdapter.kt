af12489bfe766c23defb6990771df4fd2ff49080
JoyceZhang <56086743+JZBlank@users.noreply.github.com>
2022-03-30 15:58:08 -0400

    Created Post and PostAdapter class (getting errors)

---
+++ app/src/main/java/com/mihad/artistblock/PostAdapter.kt
@@ -0,0 +1,51 @@
+package com.mihad.artistblock
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView
+import androidx.recyclerview.widget.RecyclerView
+
+class PostAdapter(val context: Context, val posts:List<Post>)
+    : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostAdapter.ViewHolder {
+        //Specify layout file to use
+
+        val view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PostAdapter.ViewHolder, position: Int) {
+        val post = posts.get(position)
+        holder.bind(post)
+    }
+
+    override fun getItemCount(): Int {
+        return posts.size
+    }
+
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val tvUsername: TextView
+        val ivImage: ImageView
+        val tvDescription: TextView
+
+
+        init {
+            tvUsername = itemView.findViewById(R.id.tvUserName)
+            ivImage = itemView.findViewById(R.id.ivImage)
+            tvDescription = itemView.findViewById(R.id.tvDescription)
+        }
+
+        fun bind(post: Post){
+            tvDescription.text = post.getDescription()
+            tvUsername.text = post.getUser()?.username
+
+            Glide.with(itemView.context).load(post.getImage()?.url).into(ivImage)
+        }
+    }
+}
